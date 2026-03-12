27735c0a7aa06e3dbbcc2afa523dcaf1f00da7e2
Lucas Vieira <lucasvtiradentes@gmail.com>
2026-03-12 00:17:55 -0300

    refactor: remove orphaned constants and enums
    Remove BASE_BRANCH, NOT_GIT_REPO_MESSAGE, GitFileStatus enum

--- src/common/constants/enums.ts
+++ src/common/constants/enums.ts
@@ -2,14 +2,6 @@ export enum ConfigKey {
   Tasks = 'tasks',
 }
 
-export enum GitFileStatus {
-  Added = 'A',
-  Modified = 'M',
-  Deleted = 'D',
-  Renamed = 'R',
-  Copied = 'C',
-}
-
 export enum ToggleLabel {
   On = 'ON',
   Off = 'OFF',
